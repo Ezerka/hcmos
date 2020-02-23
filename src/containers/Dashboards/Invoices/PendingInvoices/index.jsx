b4f799afd4852f051fa854ab3e7c872d9e72ed32
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-02-24 00:21:00 +0530

    Changed the pending pages to solve the issue

--- src/containers/Dashboards/Invoices/PendingInvoices/index.jsx
+++ src/containers/Dashboards/Invoices/PendingInvoices/index.jsx
@@ -1,38 +1,20 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { Col, Container, Row } from 'reactstrap';
-import { Button } from '@material-ui/core';
-import AddIcon from '@material-ui/icons/Add';
-import { ExamsTable, NewExamForm } from './components';
 
-const PendingInvoices = () => {
-  const [isFormOpen, setFormDialog] = useState(false);
-  const toggleNewExamForm = () => {
-    setFormDialog(!isFormOpen);
-  };
-  return (
-    <div>
-      <Container className="dashboard">
-        <Row style={{ paddingBottom: '15px' }}>
-          <Col sm={6} md={8} lg={10} xl={10}>
-            <h3 className="page-title">Pending Invoices</h3>
-            <h3 className="page-subhead subhead">
-              Here, we have all the pending issues for the system
-            </h3>
-          </Col>
-          <Col sm={6} md={4} lg={2} xl={2}>
-            <Button
-              startIcon={<AddIcon />}
-              variant="contained"
-              color="secondary"
-              onClick={toggleNewExamForm}
-            >
-              New Exam
-            </Button>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
-};
+class PendingInvoices extends Component {
+  render() {
+    return (
+      <div>
+        <Container className="dashboard">
+          <Row>
+            <Col md={12}>
+              <h3 className="page-title">Pending Invoices</h3>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default PendingInvoices;
--- src/containers/Dashboards/Issues/PendingIssues/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as ExamsTable } from './ExamsTableComponent/ExamsTable';
-export { default as NewExamForm } from './NewExamForm/index';
