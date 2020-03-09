c066a63b210b0f56b540662a77331a92dc59afc8
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-03-10 00:23:54 +0530

    Updated the login form to handle the new store

--- src/containers/Account/LogIn/components/LoginCard.jsx
+++ src/containers/Account/LogIn/components/LoginCard.jsx
@@ -1,37 +1,19 @@
 import React, { useState } from 'react';
 import LogInForm from './LogInForm';
-import logo from '../../../images/adani.png';
-import { useDispatch } from 'react-redux';
-import { history } from '../../../redux/store';
-import { loginError, loginSuccess } from '../../../redux/actions/authActions';
-import Loading from '../Loading';
-import firebase from '../../../config/firebase';
-import { loginUser } from '../../../containers/Account/LogIn/loginThunk';
+import logo from '../../../../images/adani.png';
+import { useDispatch, useSelector } from 'react-redux';
+import Loading from '../../../../shared/components/Loading';
+import { loginUser } from '../loginThunk';
 
 const LoginCard = () => {
   const dispatch = useDispatch();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
+  const { loading, error } = useSelector(state => state.user);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState('');
 
   const onSubmitFireBase = ({ email, password }) => {
     event.preventDefault();
-    dispatch(loginUser(email, password, setLoading, setError));
-    setLoading(true);
-    setError('');
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then(async res => {
-        // const user = await fetchCustomer();
-        dispatch(loginSuccess({ name: res.user.email, uid: res.user.uid }));
-        setLoading(false);
-        history.push('/home');
-      })
-      .catch(error => {
-        setLoading(false);
-        dispatch(loginError(error));
-        setError(error.message);
-      });
+    dispatch(loginUser(email, password));
   };
 
   return loading ? (
