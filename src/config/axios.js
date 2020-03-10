c3e0ebc4d95214317258a32b6483b0bfd6ac2ace
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-03-10 14:17:56 +0530

    Changed the API call for the production

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000';
+if (process.env.NODE_ENV === 'development') {
+  axios.defaults.baseURL = 'http://localhost:3000';
+} else if (process.env.NODE_ENV === 'production') {
+  axios.defaults.baseURL = 'https://adani-invoicing.herokuapp.com';
+}
 
 export default axios;
