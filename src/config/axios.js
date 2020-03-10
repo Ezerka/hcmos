70ab3c1031a16b592edc986f3cf9013c6df7e290
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-03-10 14:29:28 +0530

    FIxed the local api link error

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 if (process.env.NODE_ENV === 'development') {
-  axios.defaults.baseURL = 'http://localhost:3000';
+  axios.defaults.baseURL = 'http://localhost:8080';
 } else if (process.env.NODE_ENV === 'production') {
   axios.defaults.baseURL = 'https://adani-invoicing.herokuapp.com';
 }
